e078ece97106db143425b8b2d97c9d379be0d22e
Jportov <joaovportog@gmail.com>
2024-06-06 18:59:38 -0300

    first commit

---
+++ Exercicio/exercicio.ts
@@ -0,0 +1,11 @@
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+// Exemplos de uso
+console.log(multiplicar(3, 4)); // Saída: 12
+console.log(saudacao("Maria")); // Saída: Olá Maria
